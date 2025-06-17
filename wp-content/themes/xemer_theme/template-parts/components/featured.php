994decdf5dc7055ad44c963ccd477c46912db412
longpv <long.phamvan@adamosoft.com>
2025-06-17 09:15:16 +0700

    coding

---
+++ wp-content/themes/xemer_theme/template-parts/components/featured.php
@@ -0,0 +1,24 @@
+<div class="py-mb py-lg-pc">
+    <div class="container">
+        <div class="heading">
+            <h2 class="heading__title">
+                Về chúng tôi
+            </h2>
+            <div class="heading__desc">
+                Với hơn 10 năm kinh nghiệm, chúng tôi là đối tác tin cậy của hàng trăm doanh nghiệp trong việc chuyển đổi số và phát triển công nghệ
+            </div>
+        </div>
+
+        <div class="featured">
+            <div class="row">
+                <div class="col-md-6 col-lg-4">
+                    <div class="featured__item">
+                        <div class="featured__img">
+                            
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
